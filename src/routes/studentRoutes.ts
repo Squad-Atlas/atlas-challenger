204fc1279640e86f23c9ccc54118697d61d953da
Ana Carolina D. C <76822093+devartes@users.noreply.github.com>
2023-08-22 15:23:34 -0300

    feat: add student routes

---
+++ src/routes/studentRoutes.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import {  getStudents , createStudent, updateStudent , deleteStudent  } from "../controllers/studentController";
+
+const router = express.Router();
+
+router.post("/students", createStudent);
+router.put("/students/:id", updateStudent);
+router.get("/students", getStudents);
+router.delete("/students/:id", deleteStudent); 
+
+export default router;
