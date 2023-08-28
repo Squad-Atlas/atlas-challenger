e6ddeb3d5da7eb4d61b1e49830e3faf5e42470e4
GustavoReisSouzaLima <gustavo.lima.pb@compasso.com.br>
2023-08-28 17:10:57 -0300

    feat: add setup config swagger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import app from "@/app";
 import { config } from "@/config/config";
 import "./database/database";
+import swaggerDocs from "@/config/swagger";
 
 const PORT = config.server.port;
 
 app.listen(PORT, () => {
   console.log(`👂 Server is listening on http://localhost:${PORT} `);
+  swaggerDocs(app, PORT);
 });
