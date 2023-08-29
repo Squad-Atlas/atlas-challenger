5c1f8a3a4a643f0a26c882cd35aed93e01cb8aeb
GustavoReisSouzaLima <gustavo.lima.pb@compasso.com.br>
2023-08-29 11:21:54 -0300

    feat: encrypting passwords with bcrypt

--- src/models/instructor.ts
+++ src/models/instructor.ts
@@ -1,4 +1,5 @@
 import mongoose, { Document, Schema, Types } from "mongoose";
+import bcrypt from "bcrypt";
 
 export interface Instructor extends Document {
   name: string;
@@ -24,6 +25,19 @@ const InstructorSchema = new Schema<Instructor>(
   },
   { timestamps: true },
 );
+
+InstructorSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+InstructorSchema.methods.comparePassword = async function (
+  reqPassword: string,
+) {
+  const isMatch = await bcrypt.compare(reqPassword, this.password);
+  return isMatch;
+};
 
 const InstructorModel = mongoose.model<Instructor>(
   "Instructor",
--- src/models/student.ts
+++ src/models/student.ts
@@ -1,6 +1,7 @@
 import mongoose, { Document, Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
-interface Student extends Document {
+export interface Student extends Document {
   name: string;
   email: string;
   phone: string;
@@ -24,6 +25,19 @@ const studentSchema = new Schema<Student>(
   },
   { timestamps: true },
 );
+
+studentSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+studentSchema.methods.comparePassword = async function (
+  canditatePassword: string,
+) {
+  const isMatch = await bcrypt.compare(canditatePassword, this.password);
+  return isMatch;
+};
 
 const StudentModel = mongoose.model<Student>("Student", studentSchema);
 
