281b6175f64047311f8afe92c2c433ecd38070f7
GustavoReisSouzaLima <gustavo.lima.pb@compasso.com.br>
2023-08-29 11:19:12 -0300

    feat: add middleware error handling

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import { Request, Response } from "express";
 import { router as studentRoutes } from "@/routes/studentRoutes";
 import { router as instructorRoutes } from "@/routes/instructorRoutes";
 
+import { errorMiddleware } from "@/middlewares/error";
+import "express-async-errors";
+
 const app = express();
 
 app.use(express.json());
@@ -12,5 +15,7 @@ app.use("/api/v1", instructorRoutes);
 app.get("/", (_req: Request, res: Response) => {
   res.send("Hello World!");
 });
+
+app.use(errorMiddleware);
 
 export default app;
